7d9b568fbd49de264e5b4cbecf24e4b9c559bec9
antdece <antdecesare@gmail.com>
2021-04-28 23:01:43 -0500

    add sardines and boiler plate

---
+++ Assets/scripts/globalFlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalFlock : MonoBehaviour
+{
+    public GameObject fishObj;
+    static int numFish = 10;
+    public static int tankSize = 5;
+    public static GameObject[] fish = new GameObject[numFish];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numFish; i++){
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize));
+            fish[i] = (GameObject) Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
